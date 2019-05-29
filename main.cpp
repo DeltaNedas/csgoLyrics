c8fa541f89ee873221198081465dece48fd96594
DeltaNedas <39013340+DeltaNedas@users.noreply.github.com>
2019-05-29 01:20:42 +0100

    done a bit of work on it, still wont compile.

--- main.cpp
+++ main.cpp
@@ -23,68 +23,70 @@ int main(int argc, char* argv[]) {
 	ofstream myfile;
 
 	//Initialize Variables
-	vector<string, max> line;
-	int numOfLines = 0;
+	set<string> lines;
 	int lineNum = 0;
 	string key = "MOUSE3"; //Default key to be bound
-	string nextLine;
-	string fileName;
-	
+	string inputName;
+	string outputName;
+
 	//Creates name for file in filename variable
-	cin >> filename;
-	filename = filename+".cfg";
-	cout << "File will be called " << filename << endl << endl;
-	
-	
+
 	//Loads whatever is in lyrics.txt into array line by line
 	//Simultaneously counts number of lines in lyrics.txt
-	ifstream lyrics ("lyrics.txt");
-	for(int i = 1; lyrics.good(); i++)
-	{
-	    getline(lyrics, line[i-1]);
-	    numOfLines++;
-	}	
-	cout << "Found " << numOfLines << " lines" << endl;
-	
+	if (inputName.empty()) {
+		char buffer[128];
+		while (scanning) {
+			memset(buffer, 0, sizeof buffer);
+			scanf("128%s", &buffer);
+			lines.insert(string(buffer));
+		}
+	} else {
+		ifstream lyrics(inputName.c_str());
+		for(int i = 1; lyrics.good(); i++) {
+			string line;
+			getline(lyrics, line);
+			lines.insert(line);
+		}
+	}
+
+	printf("Found %d lines.\n", lines.size());
+
 	//Creates new text file with given name
 	//If it already exists, this erases it's contents
-	myfile.open (filename);
-	myfile << "";
-	myfile.close();
-	
+	outputFile.open(outputName);
+	if (!outputFile.is_open()) {
+		printf("Failed to open %s for reading: %d.\n", outputName, errno);
+		return errno;
+	}
+	outputFile << "";
+	outputFile.close();
+
 	//Prints initial script info into file
-	myfile.open (filename, fstream::out | fstream::app);
-	myfile << "bind " + key + " \"next_line\"" << endl;
-	myfile << "alias c0 \"alias next_line c1;\"" << endl;
-	
+	outputFile.open (fileName, fstream::out | fstream::app);
+	outputFile << "bind " + key + " \"next_line\"\n";
+	outputFile << "alias c0 \"alias next_line c1;\"\n";
+
 	//Prints each iteration for however many lines were found
-	for (int i=1; i<=numOfLines; i++)
-    {	
-    	string nextline = line[lineNum];
-  		myfile << "alias c" << i << " \"say " << nextline << "; alias next_line c" << i+1 << ";\"" << endl;
-		lineNum++;	
+	char buffer[128]
+	set<string>::iterator iter = lines.begin();
+	for (int i = 0; i < lines.size(); i++) {
+		memset(buffer, 0, sizeof buffer);
+		sprintf(&buffer, "alias c%d \"say %s; alias next_line c%d;\"\n", i + 1, *iter, linei + 2);
+		myfile << buffer;
+		advance(iter, 1)
 	}
-	
-	//Adds blank aliases to nullify any previously-loaded lyric scripts
-	for (int i = numOfLines+1; i<=max; i++){
-		myfile << "alias c" << i << " \"alias next_line c" << i+1 << ";\"" << endl;
-	}
+
 	//Prints c0 at end to be executed
 	myfile << "c0;";
-	
+
 	//Close File
 	myfile.close();
-	
+
 	//Write Success
 	cout << "Finished." << endl << endl;
 	cout << "Place your new file into the \"cfg\" folder in your CSGO directory." << endl;
 	cout << "To use, open console in game and type \"exec YourFileName.cfg\"" << endl;
 	cout << "Press MOUSE3 (Middle Mouse) repeatedly to display lines of text."  << endl << endl << endl;
-	
-	//Pause
-	string pause;
-	cout << "Done! You can now close the program." << endl;
-	cin >> pause;
-	
+
 	return 0;
 }
